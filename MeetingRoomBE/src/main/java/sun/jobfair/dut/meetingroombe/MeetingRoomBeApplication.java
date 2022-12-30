fcd374f63fb6879c964ebb027188b891ce69df0d
lehoanglong12345 <lelong.ll32@gmail.com>
2022-12-30 14:58:31 +0700

    Initialize project

---
+++ MeetingRoomBE/src/main/java/sun/jobfair/dut/meetingroombe/MeetingRoomBeApplication.java
@@ -0,0 +1,13 @@
+package sun.jobfair.dut.meetingroombe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBeApplication.class, args);
+	}
+
+}
---
+++ MeetingRoomBE/src/test/java/sun/jobfair/dut/meetingroombe/MeetingRoomBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sun.jobfair.dut.meetingroombe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
